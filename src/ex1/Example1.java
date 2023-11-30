f2ea44eb67442af7902b7c22ecfd33485529ee6e
pork74 <pork74@hanmail.net>
2023-11-30 09:13:53 +0900

    modified

--- src/ex1/Example1.java
+++ src/ex1/Example1.java
@@ -2,6 +2,6 @@ package ex1;
 
 public class Example1 {
 	public static void main(String[] args) {
-	system.out.println('오늘은 목요일입니다.');
+	System.out.println("오늘은 목요일입니다.");
 	}
 }
---
+++ src/ex1/e.java
@@ -0,0 +1,10 @@
+package ex1;
+
+public class e {
+
+	public static void main(String[] args) {
+		System.out.println("오늘은 토요일입니다.");
+
+	}
+
+}
